217cc7ef759207344fe5e26b6bef000bc488d740
phithai <phithai289@gmail.com>
2020-07-16 12:04:30 +0700

    #add string.js

---
+++ js/String.js
@@ -0,0 +1,6 @@
+var string = "I want to Learning Javascript";
+var newString = string.toLowerCase(string);
+newString = string.replace("java","hi");
+newString = string.split(" ");
+var joinedString = newString.concat(string);
+newString = string.slice(3,5);
